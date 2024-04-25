ca20770134a6d66b829ca2454faef0e03845c6b0
1trickPwnyta <sarahpace37@gmail.com>
2024-04-24 19:11:58 -0700

    Fix devourer water assault bug

---
+++ 1.5/Source/DevourerWaterAssaultFix/IncidentWorker_DevourerWaterAssaultFix.cs
@@ -0,0 +1,10 @@
+﻿namespace RimWorld
+{
+    public class IncidentWorker_DevourerWaterAssaultFix : IncidentWorker_DevourerWaterAssault
+    {
+        protected override bool CanFireNowSub(IncidentParms parms)
+        {
+            return base.CanFireNowSub(parms) && PawnsArrivalModeDefOf.EmergeFromWater.Worker.TryResolveRaidSpawnCenter(parms);
+        }
+    }
+}
