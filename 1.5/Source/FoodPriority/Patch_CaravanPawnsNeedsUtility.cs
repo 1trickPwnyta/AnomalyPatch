0d3bc07a5624630904334238bcc449fb92ac8ca0
1trickPwnyta <sarahpace37@gmail.com>
2024-05-06 22:01:48 -0700

    Add caravan meat prioritization and ghoul hunting

---
+++ 1.5/Source/FoodPriority/Patch_CaravanPawnsNeedsUtility.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+
+namespace AnomalyPatch.FoodPriority
+{
+    [HarmonyPatch(typeof(CaravanPawnsNeedsUtility))]
+    [HarmonyPatch(nameof(CaravanPawnsNeedsUtility.GetFoodScore))]
+    [HarmonyPatch(new[] { typeof(ThingDef), typeof(Pawn), typeof(float) })]
+    public static class Patch_CaravanPawnsNeedsUtility_GetFoodScore
+    {
+        public static void Postfix(ThingDef food, Pawn pawn, ref float __result)
+        {
+            if (!pawn.RaceProps.Humanlike || pawn.IsGhoul)
+            {
+                if (food == ThingDefOf.Meat_Twisted)
+                {
+                    __result += 0.2f;
+                }
+                else if (food == ThingDefOf.Meat_Human || food == ThingDef.Named("Meat_Megaspider"))
+                {
+                    __result += 0.1f;
+                }
+            }
+        }
+    }
+}
---
+++ 1.5/Source/GhoulHunting/Patch_FoodUtility.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using Verse;
+
+namespace AnomalyPatch.GhoulHunting
+{
+    [HarmonyPatch(typeof(FoodUtility))]
+    [HarmonyPatch(nameof(FoodUtility.TryFindBestFoodSourceFor))]
+    public static class Patch_FoodUtility_TryFindBestFoodSourceFor
+    {
+        public static void Postfix(Pawn getter, Pawn eater, ref Thing foodSource, ref ThingDef foodDef, bool forceScanWholeMap, ref bool __result)
+        {
+            if (!__result && getter == eater && eater.IsGhoul)
+            {
+                Pawn pawn = (Pawn)typeof(FoodUtility).GetMethod("BestPawnToHuntForPredator", BindingFlags.Static | BindingFlags.NonPublic, null, new[] { typeof(Pawn), typeof(bool) }, null).Invoke(null, new object[] { getter, forceScanWholeMap });
+                if (pawn != null)
+                {
+                    foodSource = pawn;
+                    foodDef = FoodUtility.GetFinalIngestibleDef(foodSource, false);
+                    __result = true;
+                }
+            }
+        }
+    }
+}
