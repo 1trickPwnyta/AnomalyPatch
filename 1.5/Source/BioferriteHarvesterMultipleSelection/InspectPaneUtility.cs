7a2a78a21a61adcd042274cf95f5a928660a46aa
1trickPwnyta <sarahpace37@gmail.com>
2024-05-08 11:16:45 -0700

    Fix world map inspect pane bug

--- 1.5/Source/BioferriteHarvesterMultipleSelection/InspectPaneUtility.cs
+++ 1.5/Source/BioferriteHarvesterMultipleSelection/InspectPaneUtility.cs
@@ -8,42 +8,44 @@ namespace AnomalyPatch.BioferriteHarvesterMultipleSelection
 {
     public static class InspectPaneUtility
     {
+        public static bool MultipleBioferriteHarvestersSelected()
+        {
+            return Find.Selector.NumSelected > 1 && Find.Selector.SelectedObjects.All(obj => obj is Thing && ((Thing)obj).def == ThingDefOf.BioferriteHarvester);
+        }
+
         public static void DoPaneContentsForMultipleBioferriteHarvesters(Rect rect)
         {
-            if (Find.Selector.NumSelected > 1 && Find.Selector.SelectedObjects.All(obj => obj is Thing && ((Thing)obj).def == ThingDefOf.BioferriteHarvester))
-            {
-                Rect rect2 = rect.AtZero();
-                rect2.yMin += 26f;
+            Rect rect2 = rect.AtZero();
+            rect2.yMin += 26f;
 
-                try
+            try
+            {
+                Widgets.BeginGroup(rect2);
+                string text = "BioferriteHarvesterContained".Translate() + ": ";
+                float contained = 0f;
+                float perDay = 0f;
+                foreach (object obj in Find.Selector.SelectedObjects)
                 {
-                    Widgets.BeginGroup(rect2);
-                    string text = "BioferriteHarvesterContained".Translate() + ": ";
-                    float contained = 0f;
-                    float perDay = 0f;
-                    foreach (object obj in Find.Selector.SelectedObjects)
-                    {
-                        contained += ((Building_BioferriteHarvester)obj).containedBioferrite;
-                        perDay += (float)AnomalyPatchRefs.m_Building_BioferriteHarvester_get_BioferritePerDay.Invoke(obj, new object[] { });
-                    }
-                    text += contained.ToString("0.00");
-                    text += $" (+{perDay.ToString("0.00")} {"BioferriteHarvesterPerDay".Translate()})";
-                    InspectPaneFiller.DrawInspectString(text, rect2);
+                    contained += ((Building_BioferriteHarvester)obj).containedBioferrite;
+                    perDay += (float)AnomalyPatchRefs.m_Building_BioferriteHarvester_get_BioferritePerDay.Invoke(obj, new object[] { });
                 }
-                catch (Exception ex)
+                text += contained.ToString("0.00");
+                text += $" (+{perDay.ToString("0.00")} {"BioferriteHarvesterPerDay".Translate()})";
+                InspectPaneFiller.DrawInspectString(text, rect2);
+            }
+            catch (Exception ex)
+            {
+                Debug.Log(string.Concat(new object[]
                 {
-                    Debug.Log(string.Concat(new object[]
-                    {
-                    "Error in DoPaneContentsForMultipleBioferriteHarvesters ",
-                    Find.Selector.FirstSelectedObject,
-                    ": ",
-                    ex.ToString()
-                    }));
-                }
-                finally
-                {
-                    Widgets.EndGroup();
-                }
+                "Error in DoPaneContentsForMultipleBioferriteHarvesters ",
+                Find.Selector.FirstSelectedObject,
+                ": ",
+                ex.ToString()
+                }));
+            }
+            finally
+            {
+                Widgets.EndGroup();
             }
         }
     }
---
+++ 1.5/Source/BioferriteHarvesterMultipleSelection/Patch_MainTabWindow_Inspect.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+
+namespace AnomalyPatch.BioferriteHarvesterMultipleSelection
+{
+    [HarmonyPatch(typeof(MainTabWindow_Inspect))]
+    [HarmonyPatch("get_ShouldShowPaneContents")]
+    public static class Patch_MainTabWindow_Inspect_get_ShouldShowPaneContents
+    {
+        public static void Postfix(ref bool __result)
+        {
+            __result |= InspectPaneUtility.MultipleBioferriteHarvestersSelected();
+        }
+    }
+
+    [HarmonyPatch(typeof(MainTabWindow_Inspect))]
+    [HarmonyPatch(nameof(MainTabWindow_Inspect.DoPaneContents))]
+    public static class Patch_MainTabWindow_Inspect_DoPaneContents
+    {
+        public static bool Prefix(Rect rect)
+        {
+            if (InspectPaneUtility.MultipleBioferriteHarvestersSelected())
+            {
+                InspectPaneUtility.DoPaneContentsForMultipleBioferriteHarvesters(rect);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
