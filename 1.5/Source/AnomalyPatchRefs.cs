d4e1cc59b6e8a87435f3596cb874a170caec4062
1trickPwnyta <sarahpace37@gmail.com>
2024-05-19 17:46:56 -0700

    Show character highlights in unnatural darkness

--- 1.5/Source/AnomalyPatchRefs.cs
+++ 1.5/Source/AnomalyPatchRefs.cs
@@ -12,6 +12,7 @@ namespace AnomalyPatch
     {
         public static readonly FieldInfo f_Map_spawnedThings = AccessTools.Field(typeof(Map), nameof(Map.spawnedThings));
 
+        public static readonly MethodInfo m_ModsConfig_get_AnomalyActive = AccessTools.Method(typeof(ModsConfig), "get_AnomalyActive");
         public static readonly MethodInfo m_Find_get_Maps = AccessTools.Method(typeof(Find), "get_Maps");
         public static readonly MethodInfo m_MapUtility_GetPlayerOccupiedMaps = AccessTools.Method(typeof(MapUtility), nameof(MapUtility.GetPlayerOccupiedMaps));
         public static readonly MethodInfo m_IInspectPane_get_ShouldShowPaneContents = AccessTools.Method(typeof(IInspectPane), "get_ShouldShowPaneContents");
---
+++ 1.5/Source/CharacterHighlighting/Patch_SilhouetteUtility.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using Verse;
+
+namespace AnomalyPatch.CharacterHighlighting
+{
+    [HarmonyPatch(typeof(SilhouetteUtility))]
+    [HarmonyPatch("ShouldHighlightInt")]
+    public static class Patch_SilhouetteUtility
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (CodeInstruction instruction in instructions)
+            {
+                if (instruction.opcode == OpCodes.Call && (MethodInfo)instruction.operand == AnomalyPatchRefs.m_ModsConfig_get_AnomalyActive)
+                {
+                    CodeInstruction newInstruction = new CodeInstruction(OpCodes.Ldc_I4_0);
+                    newInstruction.labels.AddRange(instruction.labels);
+                    yield return newInstruction;
+                    continue;
+                }
+
+                yield return instruction;
+            }
+        }
+    }
+}
