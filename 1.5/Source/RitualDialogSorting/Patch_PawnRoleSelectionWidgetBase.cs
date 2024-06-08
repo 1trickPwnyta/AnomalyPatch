62b3ed778d7af82c93f8201a62a93949f12a45a8
1trickPwnyta <sarahpace37@gmail.com>
2024-06-07 19:31:39 -0700

    Sort psychic ritual pawns by psy sensitivity

---
+++ 1.5/Source/RitualDialogSorting/Patch_PawnRoleSelectionWidgetBase.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace AnomalyPatch.RitualDialogSorting
+{
+    [HarmonyPatch(typeof(PawnRoleSelectionWidgetBase<PsychicRitualRoleDef>))]
+    [HarmonyPatch("DrawRoleGroup_NewTemp")]
+    public static class Patch_PawnRoleSelectionWidgetBase_DrawRoleGroup_NewTemp
+    {
+        public static void Prefix(PawnRoleSelectionWidgetBase<PsychicRitualRoleDef> __instance, ref IEnumerable<Pawn> selectedPawns)
+        {
+            if (__instance is PawnPsychicRitualRoleSelectionWidget)
+            {
+                selectedPawns = selectedPawns.OrderByDescending(p => p.GetStatValue(StatDefOf.PsychicSensitivity));
+            }
+        }
+    }
+}
