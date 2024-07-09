ae5c9223b8951b4ad9ef9b8f69cc154cee9327a8
1trickPwnyta <sarahpace37@gmail.com>
2024-07-09 12:42:25 -0700

    Don't show friendly shamblers in schedule tab

---
+++ 1.5/Source/ScheduleTabFix/Patch_MainTabWindow_Schedule.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace AnomalyPatch.ScheduleTabFix
+{
+    [HarmonyPatch(typeof(MainTabWindow_Schedule))]
+    [HarmonyPatch("get_Pawns")]
+    public static class Patch_MainTabWindow_Schedule
+    {
+        public static void Postfix(ref IEnumerable<Pawn> __result)
+        {
+            if (AnomalyPatchSettings.ScheduleTabFix)
+            {
+                __result = __result.Where(p => p.timetable != null || (p.playerSettings != null && p.playerSettings.SupportsAllowedAreas));
+            }
+        }
+    }
+}
