0f5c541c988b22f5adac1d1181218bae6f9babd3
1trickPwnyta <sarahpace37@gmail.com>
2024-10-10 17:17:44 -0700

    Remove shambler doors fix

--- 1.5/Source/AnomalyPatchSettings.cs
+++ 1.5/Source/AnomalyPatchSettings.cs
@@ -24,7 +24,6 @@ namespace AnomalyPatch
         public static bool DontHideStats = true;
         public static bool ForbidMonolithCorpses = true;
         public static bool ScheduleTabFix = true;
-        public static bool ShamblerDoors = true;
         public static bool StopSuppression = true;
         public static bool HoldingPlatformAlert = true;
 
@@ -52,7 +51,6 @@ namespace AnomalyPatch
             listingStandard.CheckboxLabeled("AnomalyPatch_DontHideStats".Translate(), ref DontHideStats);
             listingStandard.CheckboxLabeled("AnomalyPatch_ForbidMonolithCorpses".Translate(), ref ForbidMonolithCorpses);
             listingStandard.CheckboxLabeled("AnomalyPatch_ScheduleTabFix".Translate(), ref ScheduleTabFix);
-            listingStandard.CheckboxLabeled("AnomalyPatch_ShamblerDoors".Translate(), ref ShamblerDoors);
             listingStandard.CheckboxLabeled("AnomalyPatch_StopSuppression".Translate(), ref StopSuppression);
             listingStandard.CheckboxLabeled("AnomalyPatch_HoldingPlatformAlert".Translate(), ref HoldingPlatformAlert);
 
@@ -79,7 +77,6 @@ namespace AnomalyPatch
             Scribe_Values.Look(ref DontHideStats, "DontHideStats", true);
             Scribe_Values.Look(ref ForbidMonolithCorpses, "ForbidMonolithCorpses", true);
             Scribe_Values.Look(ref ScheduleTabFix, "ScheduleTabFix", true);
-            Scribe_Values.Look(ref ShamblerDoors, "ShamblerDoors", false); // should default to false if mod already active when update happens
             Scribe_Values.Look(ref StopSuppression, "StopSuppression", true);
             Scribe_Values.Look(ref HoldingPlatformAlert, "HoldingPlatformAlert", true);
         }
