fef0c21b01f8f326275629d122a6b2685caab4d7
1trickPwnyta <sarahpace37@gmail.com>
2025-01-08 10:21:17 -0800

    Show carried ghouls in colonist bar

---
+++ 1.5/Source/DeadGhoulsInColonistBar/Patch_Pawn.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace AnomalyPatch.DeadGhoulsInColonistBar
+{
+    [HarmonyPatch(typeof(Pawn))]
+    [HarmonyPatch("get_IsColonyMutantPlayerControlled")]
+    public static class Patch_Pawn
+    {
+        public static void Postfix(Pawn __instance, ref bool __result)
+        {
+            if (AnomalyPatchSettings.DeadGhoulsInColonistBar && !__result && __instance.IsColonyMutant && __instance.mutant.Def.canBeDrafted && __instance.CarriedBy != null)
+            {
+                __result = true;
+            }
+        }
+    }
+}
